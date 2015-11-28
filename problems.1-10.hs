0c70721009f6b40a446ac8d5ca64964559121573
Andrea Jemmett <andreajemmett@gmail.com>
2015-11-28 19:22:25 +0100

    first five problems!

---
+++ problems.1-10.hs
@@ -0,0 +1,34 @@
+-- Got from https://wiki.haskell.org/99_questions/1_to_10
+
+import Data.List
+
+-- 1. Find the last element of a list
+myLast :: [a] -> a
+myLast [] = error "Empty list given"
+myLast [e] = e
+myLast (_:tail) = myLast tail
+
+-- 2. Find the last but one element of a list.
+myButLast :: [a] -> a
+myButLast [] = error "Empty list given"
+myButLast [x] = error "Singleton list given"
+myButLast (_:tail) = if length tail == 2 then head tail else myButLast tail
+
+-- 3. Find the K'th element of a list. The first element in the list is number 1.
+elementAt :: [a] -> Int -> a
+elementAt [] _ = error "Index out of bound"
+elementAt (x:_) 1 = x
+elementAt (_:xs) k
+	| k < 1 = error "Index out of bound"
+	| otherwise = elementAt xs (k - 1)
+
+-- 4. Find the number of elements of a list.
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
+
+-- 5. Reverse a list.
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = (myReverse xs) ++ [x]
+
