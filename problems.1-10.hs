df01a7e5c4db715835c3ccc0dfbee3e9005fd8a5
Andrea Jemmett <andreajemmett@gmail.com>
2015-11-29 10:38:18 +0100

    done until 9 & setup for 10th

--- problems.1-10.hs
+++ problems.1-10.hs
@@ -30,9 +30,39 @@ myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = (myReverse xs) ++ [x]
 
--- 6. Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+-- 6. Find out whether a list is a palindrome.
+-- A palindrome can be read forward or backward; e.g. (x a m a x).
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome [] = True
 isPalindrome [_] = True
 isPalindrome (x:xs) = x == (last xs) && (isPalindrome $ init xs)
 
+-- 7. Flatten a nested list structure.
+data NestedList a = Elem a | List [NestedList a]
+flatten :: NestedList a -> [a]
+flatten (Elem a) = [a]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
+-- 8. Eliminate consecutive duplicates of list elements.
+compress :: (Eq a) => [a] -> [a]
+compress [a] = [a]
+compress (x:xs)
+	| x == (head xs) = compress xs
+	| otherwise = x : compress xs
+compress xs = xs
+
+-- 9. Pack consecutive duplicates of list elements into sublists.
+-- If a list contains repeated elements they should be placed in separate sublists.
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack [x] = [[x]]
+pack (x:xs)
+	| x `elem` (head (pack xs)) = (x:(head (pack xs))) : (tail (pack xs))
+	| otherwise = [x] : (pack xs)
+
+-- 10. Run-length encoding of a list.
+-- Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+-- Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode = error "Implement me!"
